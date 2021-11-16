dab7c0fc59f2e87d7923f11088327b0a0f5c9c6c
vlap1 <maksimova.va.i20@gmail.com>
2021-11-16 20:44:51 +0300

    file 2 end

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 <v-list-item 
           v-for="link in links"
           :key="link.title"
->
+          :to="link.url">
 <v-list-item-icon>
 <v-icon>{{ link.icon }}</v-icon>
 </v-list-item-icon>
@@ -37,6 +37,7 @@
 <v-btn 
        v-for="link in links"
        :key="link.title"
+       :to="link.url"
        text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
 </v-toolbar-items>
 </v-app-bar>
