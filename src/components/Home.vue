7972a14d358952442fdead0ac4a6e789b53b6333
vlap1 <maksimova.va.i20@gmail.com>
2021-11-17 22:33:42 +0300

    first commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Home</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+	<v-container>
+		<v-layout row>
+			<v-flex xs12>
+				<h1>Home</h1>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 <script>
 export default {
-  data () { 
-    return {
-    }   
-  }
+	data () { 
+		return {
+		} 	
+	}
 } 
 </script>
