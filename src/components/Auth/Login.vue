9bc772df6d2fb70b32b1798225e23f3f8c4fb53a
vlap1 <maksimova.va.i20@gmail.com>
2021-11-18 19:26:22 +0300

    login

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -2,46 +2,47 @@
 	<v-container fluid fill-height>
 		<v-layout align-center justify-center>
 			<v-flex xs12 sm8 md8>
-				<v-card class="elevetion-12">
-					<v-toolbar dark color="primary">
-						<v-toolbar-title>Login
-</v-toolbar-title>
-					</v-toolbar>
-					<v-card-text>
-					<v-form v-model="valid" ref="form"
-					validation>
-						<v-text-field
-							prepend-icon="mdi-account"
-							name="email"
-							label="Email"
-							type="email"
-							v-model="email"
-							:rules="emailRules">
-						</v-text-field>
-						<v-text-field  
-							prepend-icon="mdi-lock"
-							name="password"
-							label="Password"
-							type="password"
-							v-model="password"
-							:rules="passwordRules"
-						></v-text-field>
-						</v-form>
-					</v-card-text>
-					<v-card-actions>
-						<v-spacer></v-spacer>
-						<v-btn color="primary">
-Login
-</v-btn>
-					</v-card-actions>	
-				</v-card>
+			<v-card class="elevetion-12">
+			<v-toolbar dark color="primary">
+				<v-toolbar-title>Login</v-toolbar-title>
+			</v-toolbar>
+				<v-card-text>
+					<v-form v-model="valid" ref="form" validation>
+					<v-text-field
+					prepend-icon="mdi-account"
+					name="email"
+					label="Email"
+					type="email"
+					v-model="email"
+					:rules="emailRules"
+					>
+					</v-text-field>
+					<v-text-field  
+					prepend-icon="mdi-lock"
+					name="password"
+					label="Password"
+					type="password"
+					v-model="password"
+					:rules="passwordRules"
+					></v-text-field>
+					</v-form>
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn 
+					color="primary"
+					@click="onSubmit"
+					:disabled="!valid"
+					>
+						Login
+					</v-btn>
+				</v-card-actions>	
+			</v-card>
 			</v-flex>
 		</v-layout>
 	</v-container>
 </template>
-
 <script>
-	
 export default {
 	data () { 
 		return {
@@ -49,16 +50,28 @@ export default {
 			password: "",
 			valid: false,
 			emailRules: [
-				v => !!v || 'E-mail is required',
-			v => /.+@.+\..+/.test(v) || 'E-mail must 					be valid'
+			v => !!v || 'E-mail is required',
+	v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
 			],
 			passwordRules: [
-				v => !!v || 'Name is required',
-			v => (v && v.length >= 6) || 'Password 					must be more or equel than 6 							characters',
-			]
+	        v => !!v ||'Password is required',
+	        v => (v && v.length >= 6) ||'Password must be more or equel than 6 characters'
+	        ]
 		} 	
+	},
+	methods: {
+		onSubmit(){
+			if (this.$refs.form.validate()){
+				const user = {
+					email: this.email,
+					password: this.password
+				}
+				console.log(user)
+			}
+		}
 	}
 } 
 </script>
+
 
 
