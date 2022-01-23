9cf1d4bc1a0da036e9d2453afea791ea0502e937
vlap1 <maksimova.va.i20@gmail.com>
2022-01-24 01:04:50 +0300

    registerUser

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,38 +1,39 @@
-<template>
+<template> 
 	<v-container fluid fill-height>
-		<v-layout align-center justify-center>
-			<v-flex xs12 sm8 md8>
+		<v-layout align-center justify-center> 
+		<v-flex xs12 sm8 md8>
 			<v-card class="elevetion-12">
-			<v-toolbar dark color="primary">
-				<v-toolbar-title>Registration</v-toolbar-title>
-			</v-toolbar>
+				<v-toolbar dark color="primary">
+					Registration
+				</v-toolbar>
 				<v-card-text>
-					<v-form v-model="valid" ref="form" validation>
-					<v-text-field
-					prepend-icon="mdi-account"
-					name="email"
-					label="Email"
-					type="email"
-					v-model="email"
-					:rules="emailRules"
-					>
-					</v-text-field>
-					<v-text-field  
-					prepend-icon="mdi-lock"
-					name="password"
-					label="Password"
-					type="password"
-					v-model="password"
-					:rules="passwordRules"
-					></v-text-field>
-					<v-text-field  
-					prepend-icon="mdi-lock"
-					name="confirm-password"
-					label="Confirm Password"
-					type="password"
-					v-model="confirmPassword"
-					:rules="confirmPasswordRules"
-					></v-text-field>
+					<v-form v-model="valid" ref="form" lazy-validation>
+						<v-text-field
+						prepend-icon="mdi-account"
+						name="email"
+						label="Email"
+						type="email"
+						v-model="email"
+						:rules="emailRules"
+						></v-text-field>
+						<v-text-field
+                        prepend-icon="mdi-lock" 
+                        name="password" 
+                        label="Password" 
+                        type="password"
+                        v-model="password"
+                        :rules="passwordRules"
+                        >
+                        </v-text-field>
+                        <v-text-field
+                        prepend-icon="mdi-lock" 
+                        name="confirm-password" 
+                        label="Confirm Password" 
+                        type="password"
+                        v-model="confirmPassword"
+                        :rules="confirmPasswordRules"
+                        >
+                        </v-text-field>
 					</v-form>
 				</v-card-text>
 				<v-card-actions>
@@ -41,32 +42,31 @@
 					color="primary"
 					@click="onSubmit"
 					:disabled="!valid"
-					>
-						Create Account
-					</v-btn>
-				</v-card-actions>	
+					>Create Account</v-btn>
+				</v-card-actions>
 			</v-card>
-			</v-flex>
-		</v-layout>
+		</v-flex>
+		</v-layout> 
 	</v-container>
 </template>
+
 <script>
 export default {
 	data () { 
 		return {
+			valid: false,
 			email: "",
 			password: "",
 			confirmPassword: "",
-			valid: false,
 			emailRules: [
 			v => !!v || 'E-mail is required',
-	v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+			v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
 			],
 			passwordRules: [
 			v => !!v || 'Password is required',
 			v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
 			],
-	confirmPasswordRules: [
+			confirmPasswordRules: [
 			v => !!v || 'Password is required',
 			v => v === this.password || 'Password should match'
 			]
@@ -74,13 +74,11 @@ export default {
 	},
 	methods: {
 		onSubmit(){
-			if (this.$refs.form.validate()){
-				const user = {
-					email: this.email,
-					password: this.password
-				}
-				console.log(user)
+			const user = {
+				email: this.email,
+				password: this.password
 			}
+			this.$store.dispatch('registerUser', user)
 		}
 	}
 } 
