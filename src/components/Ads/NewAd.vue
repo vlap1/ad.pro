a37536d80f16f959513295fa98f51ac5828771be
vlap1 <maksimova.va.i20@gmail.com>
2022-01-24 22:55:41 +0300

    UploadforAd

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,59 +1,67 @@
-<template>
+<template> 
 	<v-container>
-		<v-layout row>
+		<v-layout row> 
 			<v-flex xs12 sm6 offset-sm3>
 				<h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
 				<v-form v-model="valid" ref="form" validation>
 					<v-text-field
-					name="title"
-					label="Ad Title"
-					type="text"
-					v-model="title"
-					:rules="[ v => !!v || 'Title is required']"
+						name="title" 
+						label="Ad Title" 
+						type="text" 
+						v-model="title"
+						:rules="[ v => !!v || 'Title is required']" 
 					>
 					</v-text-field>
 					<v-textarea  
-					name="description"
-					label="Ad Description"
-					type="text"
-					v-model="description"
-					:rules="[ v => !!v || 'Description is required']"
-					class="mb-3"
+						name="description" 
+						label="Ad Description" 
+						type="text" 
+						v-model="description"
+						:rules="[ v => !!v || 'Description is required']"
+						class="mb-3" 
 					></v-textarea>
 					</v-form>
 					<v-layout  row>
-							<v-flex xs12>
+						<v-flex xs12>
 							<v-btn 
-							class="mt-3"
-							color="warning">
+								class="mt-3"
+								color="warning"
+								@click="triggerUpload">
 								Upload
 								<v-icon right dark>mdi-cloud-upload</v-icon>
 							</v-btn>
-							</v-flex>
+							<input 
+								ref="fileInput" 
+								type="file" 
+								style="display:none;" 
+								accept="image/*"
+								@change="onFileChange"
+							>
+						</v-flex>
 					</v-layout>
 					<v-layout row>
-							<v-flex xs12>
-								<img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="150" class="mt-3">
-							</v-flex>
+						<v-flex xs12>
+							<img :src="imageSrc" height="150" class="mt-3" v-if="imageSrc">
+						</v-flex>
 					</v-layout>
 					<v-layout row>
-							<v-flex xs12>
-								<v-switch
-								v-model="promo"
-								label="Ad to Promo?"
+						<v-flex xs12>
+							<v-switch
+							v-model="promo"
+							label="Ad to Promo?"
 							></v-switch>
-							</v-flex>
+						</v-flex>
 					</v-layout>
 					<v-layout row>
-							<v-flex xs12>
-								<v-spacer></v-spacer>
-								<v-btn 
+						<v-flex xs12>
+							<v-spacer></v-spacer>
+							<v-btn 
 								color="success"
 								@click="createAd"
 								:loading="loading"
-								:disabled:="!valid || loading"
-								>Create Ad</v-btn>
-							</v-flex>
+								:disabled:="!valid || !image || loading" 
+							>Create Ad</v-btn>
+						</v-flex>
 					</v-layout>
 			</v-flex>
 		</v-layout>
@@ -66,29 +74,44 @@ export default {
 			valid: false,
 			title: "",
 			description: "",
-			promo: false
+			promo: false,
+			image: null,
+			imageSrc: ""
 		} 	
 	},
 	computed: {
-loading() {
-		return this.$store.getters.loading
-	}
-},
+		loading() {
+			return this.$store.getters.loading
+		}
+	},
 	methods: {
+		triggerUpload () {
+			this.$refs.fileInput.click()
+		},  
 		createAd(){
-			if (this.$refs.form.validate()){
+			if (this.$refs.form.validate() && this.image){
 			const ad = {
 				title: this.title,
 				desc: this.description,
 				promo: this.promo,
-				src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
+				image: this.image
 			}
-			this.$store.dispatch("createAd", ad)
-						.then(() => {
-						this.$router.push("/list")
-						})
-						.catch(() => {})
+			this.$store.dispatch("createAds", ad)
+			.then(() => {
+				this.$router.push("/list")
+			})
+			.catch(() => {})
 			}
+		},
+		onFileChange (event) {
+			const file = event.target.files[0]
+			const reader = new FileReader()
+			reader.onload = e => {
+				console.log(e)
+				this.imageSrc = reader.result
+			}
+			reader.readAsDataURL(file)
+			this.image = file
 		}
 	}
 } 
--- src/store/ads.js
+++ src/store/ads.js
@@ -61,23 +61,35 @@ export default {
         async createAds ({commit, getters}, payload) {
             commit('clearError')
             commit('setLoading', true)
+            const image = payload.image
       
             try {
               const newAd = new Ad(
                 payload.title,
                 payload.desc,
                 getters.user.id,
-                payload.src,
+                "",
                 payload.promo,
                 payload.id
             )
               const fbValue = await fb.database().ref('ads').push(newAd)
+              const imageExt = image.name.slice(image.name.lastIndexOf("."))
+              
               console.log ("bbbb")
               console.log (fbValue.key)
+
+              await fb.storage().ref().child(`ads/${fbValue.key}.${imageExt}`).put(image).then(snapshot => {
+                snapshot.ref.getDownloadURL().then((downloadURL) => {
+                  const src = downloadURL
+                  fb.database().ref("ads").child(fbValue.key).update({ src })
+      
               commit('setLoading', false)
               commit('createAd', {
                 ...newAd,
-                id: fbValue.key
+                id: fbValue.key,
+                src
+                })
+              })
               })
             } catch (error) {
               commit('setError', error.message)
